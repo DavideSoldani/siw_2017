96d36c0895a77f83c6a9c5324d314b7355a9c74f
davide <davide@localhost>
2017-06-15 15:43:57 +0200

    Cancello File

--- galleria/src/main/java/it/uniroma3/repository/OperaRepository.java
+++
@@ -1,9 +0,0 @@
-package it.uniroma3.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import it.uniroma3.model.Opera;
-
-public interface OperaRepository extends CrudRepository<Opera, Long>{
-
-}
